f13505f43dddad2e30a4cf7ff1e7e29b6bc67326
denbell5 <denbell712@gmail.com>
2022-05-13 16:53:38 +0300

    create mc project

---
+++ src/mc/HomeSensors/main.c
@@ -0,0 +1,18 @@
+/*
+ * HomeSensors.c
+ *
+ * Created: 5/13/2022 4:50:46 PM
+ * Author : Denis
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
